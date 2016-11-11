f02e814027326324d162edb4d9fe0690e6204a96
Victor Axelsson <victor_axelsson@hotmail.com>
2016-11-11 16:16:12 +0100

    Creted lazy registration

--- src/se/kth/networking/java/second/Marketplace.java
+++ src/se/kth/networking/java/second/Marketplace.java
@@ -15,19 +15,39 @@ import java.util.List;
  */
 public class Marketplace implements MarketplaceInterface {
 
-    public static void main(String[] args){
-        Marketplace server = new Marketplace();
 
+    private static Registry lazyCreateRegistry(Marketplace server){
+        Registry registry = null;
+        MarketplaceInterface marketplace = null;
         try {
-            MarketplaceInterface marketplace = (MarketplaceInterface) UnicastRemoteObject.exportObject(server, 0);
-            Registry registry = LocateRegistry.getRegistry();
-            registry.bind("marketplace", marketplace);
-
+            marketplace = (MarketplaceInterface) UnicastRemoteObject.exportObject(server, 0);
         } catch (RemoteException e) {
             e.printStackTrace();
+        }
+
+        try {
+            registry = LocateRegistry.getRegistry(1099);
+            registry.bind("marketplace", marketplace);
+        } catch (RemoteException e) {
+            try {
+                registry = LocateRegistry.createRegistry(1099);
+                registry.bind("marketplace", marketplace);
+            } catch (RemoteException e1) {
+                e1.printStackTrace();
+            } catch (AlreadyBoundException e1) {
+                e1.printStackTrace();
+            }
         } catch (AlreadyBoundException e) {
             e.printStackTrace();
         }
+
+        return registry;
+    }
+
+    public static void main(String[] args){
+        Marketplace server = new Marketplace();
+        Registry registry = lazyCreateRegistry(server);
+        System.out.println("Grvy");
 
     }
 
