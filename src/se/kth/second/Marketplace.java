88bea4240e4fe1a7991828f729fcf4a84bfbb195
Victor Axelsson <victor_axelsson@hotmail.com>
2016-11-11 15:52:34 +0100

    Added basic stuff for the marketplace

---
+++ src/se/kth/second/Marketplace.java
@@ -0,0 +1,58 @@
+package se.kth.second;
+
+import se.kth.networking.java.first.Client;
+import se.kth.second.models.Item;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+
+/**
+ * Created by victoraxelsson on 2016-11-11.
+ */
+public class Marketplace implements MarketplaceInterface{
+
+    public static void main(String[] args){
+        Marketplace server = new Marketplace();
+
+        try {
+            MarketplaceInterface marketplace = (MarketplaceInterface) UnicastRemoteObject.exportObject(server, 0);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("marketplace", marketplace);
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (AlreadyBoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void registerClient(Client client) {
+
+    }
+
+    @Override
+    public void unregisterClient(Client client) {
+
+    }
+
+    @Override
+    public void sellItem(Item item) {
+
+    }
+
+    @Override
+    public void buyItem(Item item) {
+
+    }
+
+    @Override
+    public List<Item> listItems() {
+        return null;
+    }
+}
---
+++ src/se/kth/second/models/Item.java
@@ -0,0 +1,32 @@
+package se.kth.second.models;
+
+/**
+ * Created by victoraxelsson on 2016-11-11.
+ */
+public class Item {
+    String name;
+    float price;
+    String currency;
+
+    public Item(String name, float price){
+        this(name, price, "SEK");
+    }
+
+    public Item(String name, float price, String currency){
+        this.name = name;
+        this.price = price;
+        this.currency = currency;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+}
